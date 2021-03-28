0d9e679cbed01859540df6b561612014b0343ff8
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-28 22:58:19 +0100

    updated hero_story.py

--- hero_story.py
+++ hero_story.py
@@ -1,2 +1,18 @@
 # control flow hero story
 
+story1 = {
+    "start": "I can’t help thinking:",
+    "middle": "Why is 'Abbreviation'",
+    "end": "A very long word?"
+}
+
+print(story1)
+print(type(story1))
+print(story1.keys())
+print(story1.values())
+
+print(story1["start"])
+print(story1["middle"])
+print(story1["end"])
+
+story1["hero"] = "Aragorn"
